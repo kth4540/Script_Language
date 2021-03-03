3067facd99c2f659760b60076b80aae972981d3e
kth4540 <harypoteck@naver.com>
2021-03-03 16:43:09 +0900

    python_basic

---
+++ python_basic/python_basic_1.py
@@ -0,0 +1,42 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 2+2
+4
+>>> 2*3
+6
+>>> 2**3
+8
+>>> 23/7
+3.2857142857142856
+>>> 23//7
+3
+>>> 23%6
+5
+>>> 'alice'+'bob
+SyntaxError: EOL while scanning string literal
+>>> 'alice'*3
+'alicealicealice'
+>>> 'alice'+str(3)
+'alice3'
+>>> age=23
+>>> type(age)
+<class 'int'>
+>>> height=165.5
+>>> type(height)
+<class 'float'>
+>>> name='jisu'
+>>> type(name)
+<class 'str'>
+>>> a,b=100,'jisu'
+>>> a,b=b,a
+>>> a
+'jisu'
+>>> b
+100
+>>> print('hello')
+hello
+>>> print(age)
+23
+>>> print('age:',age)
+age: 23
+>>> 
---
+++ python_basic/python_basic_2.py
@@ -0,0 +1,5 @@
+#name
+
+name=input('your name:')
+length=len(name)
+print('your name"'+name+'" is',length,'characters long')
