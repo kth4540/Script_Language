f05d0c530507ef5f07af39f457d4fefaa35686e0
kth4540 <harypoteck@naver.com>
2021-03-08 12:01:24 +0900

    control_flow

---
+++ control_flow/control_flow.py
@@ -0,0 +1,47 @@
+import random
+#spam=0
+#while spam<5:
+#    print('hello world')
+#    spam=spam+1
+
+#input()
+
+#while(True):
+#    print('please type your name')
+#    name=input()
+#    if name=='your name':
+#        break
+#print('thank you')
+
+#while True:
+#    print('who are you?')
+#    name=input()
+#    if name!='joe':
+#        continue
+#    print('hello, joe. what is the password')
+#    password=input()
+#    if password=='swordfish':
+#        break
+#print('access granted')
+
+#name=''
+#while not name:
+#    name=input()
+#print('how many guests will you have')
+#NumOfGuests=int(input())
+#if NumOfGuests:
+#    print('be sure to have enough room')
+
+
+# sum=0
+# for i in range(1,100+1):
+#     sum=sum+i
+#
+# print(sum)
+
+for i in range(5):
+    print(random.randint(1,10))
+
+
+
+
